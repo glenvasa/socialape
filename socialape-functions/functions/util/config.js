bf88d9b087d3d5031be4e2f6aa9b13bd9e446f4d
Glen Vasa <glenvasa@gmail.com>
2020-11-17 17:28:31 -0500

    Create config.js

---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBfEe6ZfIF2HNoPyzkK0TO-O9g-xtExrh8",
+  authDomain: "socialape-d5d91.firebaseapp.com",
+  databaseURL: "https://socialape-d5d91.firebaseio.com",
+  projectId: "socialape-d5d91",
+  storageBucket: "socialape-d5d91.appspot.com",
+  messagingSenderId: "591494099019",
+  appId: "1:591494099019:web:265562b89bc7ea1493ddce",
+  measurementId: "G-FXEKRVBD93"
+};
